ed851887db952220601bed1a0bf3be12b4ba8944
josceno <jdamasceno414@gmail.com>
2022-11-10 16:05:37 -0300

    inicializando o asp da aplicação

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookworm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
