05110e8cd530687614e0067521c1d496ba3ea426
josceno <jdamasceno414@gmail.com>
2022-11-11 15:32:41 -0300

    Adicionando modelo categorias

---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookworm.Models
+{
+    public class Categoria
+    {   
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public DateTime Datacriacao { get; set; } = DateTime.Now;
+
+
+
+    }
+}
