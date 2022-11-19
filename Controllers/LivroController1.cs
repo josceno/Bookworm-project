9089067bac841f265e9eed4e743e8c8b752fbd28
josceno <jdamasceno414@gmail.com>
2022-11-19 15:58:52 -0300

    Adição de Listagem

---
+++ Controllers/LivroController1.cs
@@ -0,0 +1,20 @@
+﻿using Bookworm.Data;
+using Bookworm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookworm.Controllers
+{
+    public class LivroController : Controller
+    {
+        private readonly ApplicationtDbContext _db;
+
+        public LivroController(ApplicationtDbContext db){
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Livro> objLivroList = _db.Livro;
+            return View(objLivroList);
+        }
+    }
+}
